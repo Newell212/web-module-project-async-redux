e2e1ec37f65bd0ec4d775adee2abaf3001391872
Michael Newell <michael.newell212@gmail.com>
2023-05-01 17:58:18 -0600

    created store and imported boredActions to Bored

--- src/Components/Bored.js
+++ src/Components/Bored.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {boredAction} from '../actions/boredActions'
 
 
 // https://www.boredapi.com/api/activity
 
-const Bored = () => {
+const Bored = (props) => {
     return (
         <>
         <h2>Click the button to eliminate your boredom!</h2>
-        <button>Click Me!</button>
+        <button onClick={props.boredAction}>Click Me!</button>
         
         </>
     )
 }
 
+const mapStateToProps = state => {
+    return {
+        newIdea: state.newIdea
+    }
+}
 
-export default Bored
+
+export default connect(mapStateToProps, {boredAction})(Bored);
---
+++ src/actions/boredActions.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const GET_NEW_IDEA = 'GET_NEW_IDEA';
+
+
+export const boredAction = () => dispatch => {
+    axios.get('https://www.boredapi.com/api/activity')
+    .then(res => {
+        console.log(res)
+    })
+}
+
+export const getNewIdea = newIdea => ({
+    type: GET_NEW_IDEA, payload: newIdea
+})
